3dcbc762325e9cf4254d64d759ff0fc487ee185b
Madi Zhaksykeldi <madizhaksykeldi@Madis-MacBook-Air-3.local>
2024-04-02 22:03:52 +0500

    added new elements into array

--- src/main/java/StringManipulationApp.java
+++ src/main/java/StringManipulationApp.java
@@ -1,5 +1,10 @@
 public class StringManipulationApp {
     public static void main(String[] args) {
+        String[] newStringArray = {};
+        StringArrayManipulator stringArrayManipulator = new StringArrayManipulator(newStringArray);
+        stringArrayManipulator.addString("orange");
+        stringArrayManipulator.addString("apple");
+        stringArrayManipulator.addString("pineapple");
 
     }
 }
